0a66caa383c2bd0fba2072e03ea85d3d603b1c20
wonjinSong <wjsong0204@naver.com>
2024-10-30 07:50:49 +0900

    프로젝트 생성

---
+++ Capstone_DonWorry/src/main/java/com/example/donworry/CapstoneDonWorryApplication.java
@@ -0,0 +1,13 @@
+package com.example.donworry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDonWorryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneDonWorryApplication.class, args);
+	}
+
+}
---
+++ Capstone_DonWorry/src/test/java/com/example/donworry/CapstoneDonWorryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donworry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDonWorryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
