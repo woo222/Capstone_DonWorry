920652943988e72bb3827386d82e095289ff4ed8
wonjinSong <wjsong0204@naver.com>
2024-10-31 13:50:25 +0900

    Feat:로그인 회원가입 구현 시도

--- Capstone_DonWorry/src/main/java/com/example/donworry/CapstoneDonWorryApplication.java
+++ Capstone_DonWorry/src/main/java/com/example/donworry/CapstoneDonWorryApplication.java
@@ -2,8 +2,12 @@ package com.example.donworry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.donworry")
+@EntityScan(basePackages = "com.example.donworry")
 public class CapstoneDonWorryApplication {
 
 	public static void main(String[] args) {
---
+++ Capstone_DonWorry/src/main/java/com/example/donworry/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.example.donworry;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+	
+	public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+}
